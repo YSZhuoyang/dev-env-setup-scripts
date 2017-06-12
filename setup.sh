95bb96a50c6cc4e4f72dae020649048f0a33dc8d
oscar <yuszy@outlook.com>
2017-06-12 16:28:55 +1000

    Minors changesclea

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 # install vscode
-echo "install vscode ..."
+echo "\nInstall vscode ...\n"
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'

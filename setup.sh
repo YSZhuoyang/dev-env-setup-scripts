5171fbb7b2aedcd17f6d2e3bb84c2ff63a8787b6
Oscar <yuszy@outlook.com>
2017-06-12 16:26:25 +1000

    Minors changesclear

--- setup.sh
+++ setup.sh
@@ -5,30 +5,30 @@ curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microso
 sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 sudo apt update
-sudo apt install code
+sudo apt install -y code
 
 # install common used apt packages
-echo "common used apt packages ..."
-sudo apt install git vim filezilla
+echo "\n Install common used apt packages ...\n"
+sudo apt install -y git vim filezilla
 
 # install nodejs 8
-echo "install nodejs 8 ..."
+echo "\nInstall nodejs 8 ...\n"
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt install -y nodejs
 
 # download and install stacer 1.0.7 (a system cleaning app)
 # repo: https://github.com/oguzhaninan/Stacer
-echo "download and install stacer 1.0.7 (a system cleaning app) ..."
-wget https://github.com/oguzhaninan/Stacer/releases/download/v1.0.7/stacer_1.0.7_amd64.deb
+#echo "\nDownload and install stacer 1.0.7 (a system cleaning app) ...\n"
+#wget https://github.com/oguzhaninan/Stacer/releases/download/v1.0.7/stacer_1.0.7_amd64.deb
 # install dependencies
-sudo apt install gconf2 libappindicator1 libindicator7
-sudo dpkg -i stacer_1.0.7_amd64.deb
-sudo rm -r stacer_1.0.7_amd64.deb
+#sudo apt install gconf2 libappindicator1 libindicator7
+#sudo dpkg -i stacer_1.0.7_amd64.deb
+#sudo rm -r stacer_1.0.7_amd64.deb
 
 # check and install missing dependencies, cleanup
-echo "check and install missing dependencies ..."
-sudo apt install -f
-sudo apt autoremove
-sudo apt autoclean
+echo "\nCheck and install missing dependencies ...\n"
+sudo apt install -f -y
+sudo apt autoremove -y
+sudo apt autoclean -y
 
 

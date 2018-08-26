2985db61faeab66df709624ba7c212404dd8463d
oscar <yuszy@outlook.com>
2018-08-26 16:53:53 +1000

    Updated go to v1.11

--- setup.sh
+++ setup.sh
@@ -60,19 +60,19 @@ then
 
 fi
 
-# Install golang V1.10
-read -p "Do you want to install golang V1.10.2? Y/y for yes; Others for no: " ANS
+# Install golang V1.11
+read -p "Do you want to install golang V1.11? Y/y for yes; Others for no: " ANS
 if [[ $ANS == "y" || $ANS == "Y" ]]
 then
-    printf "\nInstall golang V1.10.2 ...\n\n"
-    sudo wget "https://dl.google.com/go/go1.10.2.linux-amd64.tar.gz"
-    sudo tar -C /usr/local -xzf go1.10.2.linux-amd64.tar.gz
+    printf "\nInstall golang V1.11 ...\n\n"
+    sudo wget "https://dl.google.com/go/go1.11.linux-amd64.tar.gz"
+    sudo tar -C /usr/local -xzf go1.11.linux-amd64.tar.gz
     # Setup environment
     echo "export PATH=\$PATH:/usr/local/go/bin" >> ~/.profile
     echo "export GOPATH=\$HOME/Documents/Sources/GoWorkPlace" >> ~/.profile
     echo "export GOBIN=\$GOPATH/bin" >> ~/.profile
     # Remove temp files
-    sudo rm -r go1.10.2.linux-amd64.tar.gz
+    sudo rm -r go1.11.linux-amd64.tar.gz
 fi
 
 # Install CUDA V9.2
